bb608c5852e4ed7cdf09675835a231074c9b45b2
Suhas KV <suhaskv0111@gmail.com>
2023-04-09 17:15:28 +0530

    added comments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 use std::collections::BTreeMap;
 
 fn fault_equivalence_op<T>(gate: &str, out: &str, in_pref: &str) {
+    /* Function of the fault equivalence operation */
     let mut fault_to_remove: Vec<T> = Vec::new();
 
     if gate == "AND" {
@@ -13,6 +14,7 @@ fn fault_equivalence_op<T>(gate: &str, out: &str, in_pref: &str) {
 }
 
 fn stuck_at_fault_number(num_nets: &usize) -> usize{
+    /* Function to return number of stuck-at faults */
     num_nets * 2
 }
 
@@ -20,25 +22,25 @@ fn main() {
     let netlist_file = File::open("netlist.txt").unwrap();
     let reader = BufReader::new(netlist_file);
 
-    let mut netlist_wires = Vec::new();
+    let mut netlist_wires = Vec::new(); // Vector for netlist lines
 
     for line in reader.lines() {
         netlist_wires.push(line.unwrap());
     }
     //println!("{:?}", netlist_wires);
 
-    let split_netlist: Vec<Vec<_>> = netlist_wires.iter().map(|s| s.split(" ").collect()).collect();
+    let split_netlist: Vec<Vec<_>> = netlist_wires.iter().map(|s| s.split(" ").collect()).collect(); // Vector to split the netlist line
 
     //println!("{:?}", split_netlist);
 
-    let mut nets = Vec::new();
+    let mut nets = Vec::new(); // Vector of nets
 
     for i in 0..netlist_wires.len() {
         let temp_slice = &split_netlist[i][1..4];
         nets.push(temp_slice);
     }
 
-    let mut combined_nets: Vec<_> = nets.concat();
+    let mut combined_nets: Vec<_> = nets.concat(); // Vector with all nets
     combined_nets.sort();
     combined_nets.dedup();
 
@@ -49,8 +51,9 @@ fn main() {
     num_fault = stuck_at_fault_number(&temp_num);
     println!("Number of total stuck-at faults: {}", num_fault);
 
-    let mut fault_vectors = Vec::new();
-    let mut fault_values = Vec::new();
+    let mut fault_vectors = Vec::new(); // Vector with stuck-at faults
+    let mut fault_values = Vec::new(); // Vector with stuck-at values for the fault 
+                                       // (1 -> Present, 0 -> Collapsed/Absent)
     let mut value = 0;
 
     for i in 1..combined_nets.len()+1 {
@@ -60,13 +63,15 @@ fn main() {
         fault_vectors.push(sa0);
         fault_vectors.push(sa1);
     }
+
     println!("Stuck-at faults: {:?}", fault_vectors); 
+    
     while value <= (fault_vectors.len()-1) {
         fault_values.push(1);
         value += 1;
     }
 
-    let mut fault_map = BTreeMap::new();
+    let mut fault_map = BTreeMap::new(); // BTreeMap with enum stuck-at fault and value
 
     for (i, (&ref fault_vector, &fault_value)) in fault_vectors.iter().zip(fault_values.iter()).enumerate() {
         fault_map.insert(i+1, (fault_vector, fault_value));
